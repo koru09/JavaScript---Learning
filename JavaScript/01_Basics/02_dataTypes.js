d45ff7b9ad98786a75fffc9c82add1c6c9302c68
Mr. Noya <147297458+koru09@users.noreply.github.com>
2024-03-28 03:39:35 +0000

    why null is not a object datatypes

--- JavaScript/01_Basics/02_dataTypes.js
+++ JavaScript/01_Basics/02_dataTypes.js
@@ -11,3 +11,15 @@
 
 console.table([typeof Number, typeof BigInt, typeof Boolean, typeof String, typeof Symbol, typeof null, typeof undefined]);
 console.log(typeof 23);
+
+/*
+
+In JavaScript, the fact that typeof null returns 'object' is actually a historical quirk and considered by many to be a mistake or an oversight in the language.
+
+null is not actually an object. It is a primitive value that represents the intentional absence of any object value. However, due to a bug in the original JavaScript implementation, typeof null returns 'object'.
+
+This behavior has been preserved for backward compatibility reasons, as changing it could potentially break existing code that relies on this behavior.
+
+So, while typeof null returns 'object', it's important to understand that null is not actually an object in JavaScript. It's a primitive value, along with types like numbers, strings, booleans, etc.
+
+*/ 
