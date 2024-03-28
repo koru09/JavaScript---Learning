8ac3f9abef2873fecde8126d8fa1e48ff03fcff9
Mr. Noya <147297458+koru09@users.noreply.github.com>
2024-03-28 03:35:06 +0000

    learned about datatypes in JS

---
+++ JavaScript/01_Basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict" // treat all JS code as newer version
+
+// number => 2 to power 53
+// bigint
+// boolean => true / false
+// string => ""
+
+//symbol => unique
+// null => standalone value
+// undefined =>
+
+console.table([typeof Number, typeof BigInt, typeof Boolean, typeof String, typeof Symbol, typeof null, typeof undefined]);
+console.log(typeof 23);
